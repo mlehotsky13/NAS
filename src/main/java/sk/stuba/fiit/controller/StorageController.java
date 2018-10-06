f3e875507abcebc8e61abf54e76031c40d1ac516
mlehotsky13 <mlehotsky13@gmail.com>
2018-10-06 23:39:43 +0200

    Thymeleaf setup

---
+++ src/main/java/sk/stuba/fiit/controller/StorageController.java
@@ -0,0 +1,15 @@
+package sk.stuba.fiit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class StorageController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+}
