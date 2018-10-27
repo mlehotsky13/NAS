f8a76ea9981244bb72441afdea93e9a95917645e
mlehotsky13 <mlehotsky13@gmail.com>
2018-10-27 17:26:48 +0200

    Moved script imports

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,12 +1,16 @@
-$(".clickable-row").click(function () {
-  $(".content-box-large").load("/storages/details .content-box-large >*", {
-    "path": $(this).find('td').eq(2).text()
-  }, function () {
-    $.getScript("/js/scripts.js");
+$(document).ready(function () {
+  $(".clickable-row").click(function () {
+    $(".content-box-large").load("/storages/details .content-box-large >*", {
+      "path": $(this).find('td').eq(2).text()
+    }, function () {
+      $.getScript("/js/scripts.js");
+    });
   });
 });
 
-$("a.refresh").click(function () {
-  $(".panel-body").load("/storages table");
-  window.location.reload();
+$(document).ready(function () {
+  $("a.refresh").click(function () {
+    $(".panel-body").load("/storages table");
+    window.location.reload();
+  });
 });
